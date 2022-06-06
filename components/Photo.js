7df6da23d9d768d1c8f8ef8b94806d41ffdc7d51
gaonita <gaon.yg@gmail.com>
2022-06-06 23:28:49 +0200

    photo preview with navigation

--- components/Photo.js
+++ components/Photo.js
@@ -1,21 +1,48 @@
-import React, {useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import Image from "next/image";
+
 import styles from "../styles/Home.module.css";
 
-export default function Photo({photo}){
+export default function Photo({photo, open, onClick}) {
+    useEffect(() => {
+        if (open) {
+            const rect = photoRef.current.getBoundingClientRect()
+            const x = rect.x + rect.width / 2;
+            const y = rect.y + rect.height / 2;
+            setMoveToX(window.innerWidth / 2 - x);
+            setMoveToY(window.innerHeight / 2 - y);
+        } else {
+            setMoveToX(0);
+            setMoveToY(0);
+        }
+    }, [open])
+
     const photoRef = useRef();
-    const [x, setX] = useState();
+    const [moveToX, setMoveToX] = useState(0)
+    const [moveToY, setMoveToY] = useState(0)
 
-    const movePhoto = () => {
-       setX( photoRef.current.getBoundingClientRect().x)
-    }
 
-    return(
-        <div className={styles.photo} ref={photoRef}
-             onClick={movePhoto}
-             style={{margin: 8,
-             transform:'translateX(100px)'
-             }} >
+    return (
+        photo&&
+        <div className={styles.photo}
+             ref={photoRef}
+             onClick={onClick}
+             style={{
+                 margin: 8,
+                 display:'flex',
+                 flexDirection:'column',
+                 transform: `translate(${moveToX}px, ${moveToY}px) scale(${open ? 2 : 1})`,
+                 transition: `transform 300ms linear`,
+                 zIndex: open ? 10 : 0,
+             }}>
             <img alt={'img'} src={photo.url} width={290} height={290}/>
+            <p style={{
+                padding:8,
+                fontSize: '0.5rem',
+                display: open ? "block" : "none",
+                color:'white',
+                alignSelf:'center',
+            }}>{photo.title}</p>
         </div>
     )
 }
--- pages/user/[id]/album/[albumid].js
+++ pages/user/[id]/album/[albumid].js
@@ -1,12 +1,65 @@
 import {getUsersWithAlbums} from "../../../api/api";
 import Image from "next/image";
-import React, {useRef} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import Layout from "../../../../components/Layout";
 import styles from '../../../../styles/Home.module.css'
 import Photo from "../../../../components/Photo";
-
+import closeIcon from "../../../../assets/close.svg";
+import arrowLeft from "../../../../assets/arrowLeft.svg"
+import arrowRight from "../../../../assets/arrowRight.svg"
 
 export default function AlbumDetail({user, album}) {
+
+    const [currentPhotoIndex, setCurrentPhotoIndex] = useState(-1)
+
+    const onClickPhoto = (photoIndex) => {
+        setCurrentPhotoIndex(photoIndex)
+        document.body.style.overflow = 'hidden';
+    }
+    const onClose = () => {
+        setCurrentPhotoIndex(-1)
+        document.body.style.overflow = '';
+    }
+
+    const moveToPrevious = useCallback(() => {
+        setCurrentPhotoIndex(prev => {
+            if (prev > 0) {
+                return prev - 1
+            } else {
+                alert('first photo')
+                return prev
+            }
+        })
+    }, [])
+
+    const moveToNext = useCallback(() => {
+        setCurrentPhotoIndex(prev => {
+            if (prev < album.photos.length - 1) {
+                return prev + 1
+            } else {
+                alert('last photo')
+                return prev
+            }
+        })
+    }, [album?.photos.length])
+
+    useEffect(() => {
+        window.onkeydown = (event) => {
+            switch (event.code) {
+                case "ArrowRight":
+                    moveToNext();
+                    break;
+                case "ArrowLeft":
+                    moveToPrevious();
+                    break;
+                default:
+                    break;
+            }
+        }
+        return (() => {
+            window.onkeydown = null
+        })
+    }, [moveToPrevious, moveToNext])
 
     return (
         user && album &&
@@ -25,11 +78,55 @@ export default function AlbumDetail({user, album}) {
                 <h1>{album.title}</h1>
                 <p>{album.photos.length} photos</p>
             </div>
-            <div style={{display: "flex", alignItems: 'center', justifyContent: 'space-between', flexWrap: 'wrap'}}>
+            <div style={{
+                display: currentPhotoIndex !== -1 ? "flex" : "none",
+                flexDirection: 'column',
+                position: 'fixed',
+                width: '100%',
+                height: '100%',
+                top: 0,
+                left: 0,
+                background: 'rgba(0,0,0,0.8)',
+                zIndex: 1,
+
+            }}>
+                <div style={{
+                    padding: 36,
+                    alignSelf: 'flex-end',
+                    zIndex: 2
+                }}
+                     onClick={onClose}>
+                    <Image src={closeIcon} alt={`photo`} width={36} height={36}
+                           style={{cursor: 'pointer'}}/>
+                </div>
+                <div style={{
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                    alignItems: 'center',
+                    position: 'absolute'
+                }}>
+
+                    <div style={{cursor: 'pointer'}}
+                         onClick={moveToPrevious}>
+                        <Image src={arrowLeft} alt={'left'} width={50} height={50}/>
+                    </div>
+
+                    <div style={{cursor: 'pointer'}}
+                         onClick={moveToNext}><Image src={arrowRight} alt={'left'} width={50} height={50}/>
+                    </div>
+                </div>
+            </div>
+            <div style={{display: "flex", alignItems: 'center', flexWrap: 'wrap'}}>
 
                 {
-                    album.photos.map(p =>
-                        <Photo key={p.id} photo={p}/>
+                    album.photos.map((p, i) =>
+                        <Photo
+                            key={p.id}
+                            photo={p}
+                            open={currentPhotoIndex === i}
+                            onClick={() => onClickPhoto(i)}/>
                     )
                 }
 
